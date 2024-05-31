6e7b47a73e7d3699a3c2f403180e9f68dcba4061
linxidrf <dayanaidrf@gmail.com>
2024-05-31 16:39:17 -0500

    Create header.php

---
+++ src/templates/header.php
@@ -0,0 +1,7 @@
+<?php
+
+define('TEMPLATES_URL', __DIR__ . '/templates');
+define('FUNCIONES_URL', __DIR__ . '/funciones.php');
+define('CARPETA_IMAGENES', __DIR__ . '/img');
+define('CARPETA_CSS', __DIR__ . '/css');
+define('CARPETA_JS', __DIR__ . '/js');
